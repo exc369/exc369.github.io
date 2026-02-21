5b750fad9b0b35439bf95ecfeae721a30dd6de1b
Eric Chang <nonoeric123@gmail.com>
2026-02-21 02:24:38 -0500

    updating about page and starting to update resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -18,28 +18,10 @@ function Resume({ isActive }) {
         <ol className="timeline-list">
 
           <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">University school of the arts</h4>
-            <span>2007 — 2008</span>
+            <h4 className="h4 timeline-item-title">Case Western Reserve University</h4>
+            <span>2018 — 2022</span>
             <p className="timeline-text">
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et
-              quas molestias exceptur.
-            </p>
-          </li>
-
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">New york academy of art</h4>
-            <span>2006 — 2007</span>
-            <p className="timeline-text">
-              Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..
-            </p>
-          </li>
-
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">High school of art and design</h4>
-            <span>2002 — 2004</span>
-            <p className="timeline-text">
-              Duis aute irure dolor in reprehenderit in voluptate, quila voluptas mag odit aut fugit, sed consequuntur
-              magni dolores eos.
+              B.S. in Computer Science.
             </p>
           </li>
 
@@ -59,8 +41,8 @@ function Resume({ isActive }) {
         <ol className="timeline-list">
 
           <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">Creative director</h4>
-            <span>2015 — Present</span>
+            <h4 className="h4 timeline-item-title">JPMorganChase Software Engineer II, Associate</h4>
+            <span>2025 - Present</span>
             <p className="timeline-text">
               Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas
               molestias exceptur.
@@ -68,8 +50,8 @@ function Resume({ isActive }) {
           </li>
 
           <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">Art director</h4>
-            <span>2013 — 2015</span>
+            <h4 className="h4 timeline-item-title">JPMorganChase Software Engineer I, Analyst</h4>
+            <span>2022 - 2025</span>
             <p className="timeline-text">
               Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et
               quas molestias exceptur.
@@ -77,8 +59,8 @@ function Resume({ isActive }) {
           </li>
 
           <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">Web designer</h4>
-            <span>2010 — 2013</span>
+            <h4 className="h4 timeline-item-title">JPMorganChase Software Engineering Intern</h4>
+            <span>Summer 2021</span>
             <p className="timeline-text">
               Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et
               quas molestias exceptur.
